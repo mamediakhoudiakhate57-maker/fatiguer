8eb07e005fe6fb7bc262465deff7d4a48b0c5b06
mamediakhoudiakhate <mamediakhoudiakhate57@gmail.com>
2026-03-26 22:05:38 +0000

    bakhe

--- coaching exo 1.c
+++ coaching exo 1.c
@@ -8,6 +8,14 @@ int main(){
     for(i=n;i<=10;i++);
 
     printf("les 10 entiers suivants sont :%d\n",n);
+     int i,j,k;
+      for(i=1;i<4;i++){
+        puts("Retardiste");
+        for (j=1;j<=3;j++){
+            puts("il faut le faire comme ca");
+
+        for(k=10;k>5;k--){
+         puts("les assembles");
 
 return 0;
 }
